ee48c10ac5f32a1bc7a5a749b0a20445a2d7f75d
HowardALandman <howard@riverrock.org>
2020-05-04 00:06:50 +0100

    Deleted unused top level code. Added exception handling in case SPI is not enabled or can't be opened.

--- src/tdc7201/tdc7201/__init__.py
+++ src/tdc7201/tdc7201/__init__.py
@@ -268,7 +268,15 @@ class TDC7201():
         #self.reg2 = [None for i in range(self.MAXREG24+1)]
         # Open SPI to side 1 of the chip
         # Later we should write routines 
-        self._spi.open(0,0)	# Open SPI port 0, RPi CS0 = chip CS1.
+        try:
+            self._spi.open(0,0)	# Open SPI port 0, RPi CS0 = chip CS1.
+        except FileNotFoundError:
+            print("Unable to open SPI device. Is SPI enabled?")
+            print("You can use "lsmod | grep spi" to check if any SPI kernel modules are loaded.")
+            print("If not, turn SPI on using raspi-config -> Interfacing Options -> SPI")
+            print("or Menu -> Preferences -> Raspberry Pi Configuration -> Interfaces.")
+            print("You will then need to reboot.")
+            raise RuntimeError("SPI not available")
         self.chip_select = 1
         #print("SPI interface started to tdc7201 side 1.")
         # Check all SPI settings.
@@ -642,52 +650,3 @@ class TDC7201():
             speed = self._maxSPIspeed
         print("Setting SPI clock speed to", speed/1000000, "MHz.")
         self._spi.max_speed_hz = speed
-
-# Execute if called as a program (and not if imported as a library)
-if __name__ == "__main__":
-    tdc = TDC7201()	# Create TDC object with SPI interface.
-    tdc.initGPIO()	# Set pin directions and default values for non-SPI signals.
-
-    # Setting and checking clock speed.
-    tdc.set_SPI_clock_speed(25000000)
-    #print("")
-
-    # Internal timing
-    #print("UNIX time settings:")
-    #print("Epoch (time == 0):", time.asctime(time.gmtime(0)))
-    now = time.time()
-    #print("Time since epoch in seconds:", now)
-    #print("Current time (UTC):", time.asctime(time.gmtime(now)))
-    print("Current time (local):", time.asctime(time.localtime(now)), time.strftime("%Z"))
-    #print("Time since reset asserted:", now - reset_start)
-    time.sleep(0.1)	# ensure a reasonable reset time
-    #print("Time since reset asserted:", now - reset_start)
-
-    # Turn the chip on.
-    tdc.on()
-
-    # Make sure our internal copy of the register state is up to date.
-    #print("Reading chip side #1 register state:")
-    tdc.read_regs1()
-    #tdc.print_regs1()
-
-    # Measure average time per measurement.
-    iters = 1000
-    then = time.time()
-    resultList = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    for m in range(iters):
-        resultList[tdc.measure(simulate=True)] += 1
-        # Clear interrupt register bits to prepare for next measurement.
-        tdc.clear_status()
-    print(resultList)
-    now = time.time()
-    duration = now - then
-    print(iters,"measurements in",duration,"seconds")
-    print((iters/duration),"measurements per second")
-    print((duration/iters),"seconds per measurement")
-    # Read it back to make sure.
-    #tdc.read_regs1()
-    #tdc.print_regs1()
-
-    # Turn the chip off.
-    tdc.off()
