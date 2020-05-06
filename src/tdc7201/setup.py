810c834d3d4e229513e23f063ff15209bc09834a
HowardALandman <howard@riverrock.org>
2020-05-06 15:05:16 +0100

    Set long_description_content_type to text/markdown.

--- src/tdc7201/setup.py
+++ src/tdc7201/setup.py
@@ -8,6 +8,7 @@ with open('README.md') as f:
 setup(name='tdc7201',
       version='0.2b6',
       description='Raspberry Pi driver for Texas Instruments TDC7201 Time-to-Digital-Converter chip',
+      long_description_content_type='text/markdown'
       long_description=long_description,
       install_requires=["RPi.GPIO>=0.5","spidev>=3.4"],
       # also requires "time", "sys", "random", and maybe "optparse",
