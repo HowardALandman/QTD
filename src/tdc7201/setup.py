a36c081da93040024c2c440fc18af41692378af1
HowardALandman <howard@riverrock.org>
2020-05-10 04:58:45 +0100

    Bump version, add some classifiers.

--- src/tdc7201/setup.py
+++ src/tdc7201/setup.py
@@ -6,12 +6,12 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name='tdc7201',
-      version='0.3b2',
+      version='0.4b1',
       description='Raspberry Pi driver for Texas Instruments TDC7201 Time-to-Digital-Converter chip',
       long_description_content_type='text/markdown',
       long_description=long_description,
       install_requires=["RPi.GPIO>=0.5","spidev>=3.4"],
-      # also requires "time", "sys", "random", and maybe "optparse",
+      # also requires "time", "sys", and "random",
       # but those are all built-in.
       classifiers=[
 	  "Development Status :: 4 - Beta",
@@ -21,9 +21,12 @@ setup(name='tdc7201',
           "Natural Language :: English",
 	  "Operating System :: POSIX :: Linux",
 	  "Programming Language :: Python :: 3",
+	  "Programming Language :: Python :: 3.6",
+	  "Programming Language :: Python :: 3.7",
 	  "Topic :: Scientific/Engineering",
 	  "Topic :: Scientific/Engineering :: Physics",
 	  "Topic :: Software Development :: Libraries :: Python Modules",
+	  "Topic :: System :: Hardware",
 	  "Topic :: System :: Hardware :: Hardware Drivers"
       ],
       url='https://github.com/HowardALandman/QTD/src/tdc7201',
