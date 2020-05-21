e6c9f5e3e753f9b8959bdfacc2152ab359f00807
HowardALandman <howard@riverrock.org>
2020-05-21 03:08:55 +0100

    Publish driver, os_name. Fix batch so node-red pie chart works.

--- src/qtd.py
+++ src/qtd.py
@@ -6,16 +6,27 @@ import sys
 import os
 import tdc7201
 import paho.mqtt.client as mqtt
+import json
 
 mqtt_server_name = "mqtt.eclipse.org"
 
 # MQTT callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    #print("Connected with result code "+str(rc))
+    print("Connected to", mqtt_server_name, "with result code", rc)
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.connect(mqtt_server_name, 1883, 60)
+
+def publish_tdc7201_driver():
+    command = "python3 -m pip show tdc7201 | grep Version"
+    try:
+        driver = os.popen(command).read().split()[1]
+    except:
+        driver = "unknown"
+    print("TDC7201 driver version =",driver)
+    client.publish(topic="QTD/VDGG/tdc7201/driver",payload=driver)
 
 cpu_temp_filename = "/sys/class/thermal/thermal_zone0/temp"
 gpu_temp_command = "vcgencmd measure_temp"
@@ -31,6 +42,15 @@ version = uname.version
 client.publish(topic="QTD/VDDG/"+nodename+"/nodename",payload=nodename,retain=True)
 client.publish(topic="QTD/VDDG/"+nodename+"/machine",payload=machine,retain=True)
 client.publish(topic="QTD/VDDG/"+nodename+"/OS",payload=sysname+" "+release+" "+version,retain=True)
+# Get "pretty" OS name
+try:
+    os_name = os.popen("head -1 /etc/os-release").read()
+    print("OS name =",os_name)
+    os_name = os_name.split('"')[1]
+    print("OS name =",os_name)
+    client.publish(topic="QTD/VDDG/"+nodename+"/os_name",payload=os_name,retain=True)
+except:
+    pass
 cpuinfo_filename = "/proc/cpuinfo"
 try:
     cpuinfo = open(cpuinfo_filename)
@@ -41,7 +61,9 @@ else:
     for line in hw:
         #print(line,end='')
         hw_pair = line.split()
-        client.publish(topic="QTD/VDDG/"+nodename+"/"+hw_pair[0],payload=" ".join(hw_pair[2:]),retain=True)
+        topic = "QTD/VDDG/"+nodename+"/"+hw_pair[0]
+        print(topic)
+        client.publish(topic=topic,payload=" ".join(hw_pair[2:]),retain=True)
 
 tdc = tdc7201.TDC7201()	# Create TDC object with SPI interface.
 
@@ -62,7 +84,8 @@ now = time.time()
 #print("Current time (UTC):", time.asctime(time.gmtime(now)))
 print("Current time (local):", time.asctime(time.localtime(now)), time.strftime("%Z"))
 #print("Time since reset asserted:", now - reset_start)
-time.sleep(0.1)	# ensure a reasonable reset time
+publish_tdc7201_driver()	# Takes a few seconds.
+#time.sleep(0.1)	# ensure a reasonable reset time
 #print("Time since reset asserted:", now - reset_start)
 
 # Turn the chip on.
@@ -77,11 +100,14 @@ tdc.read_regs()
 
 batches = -1	# number of batches, negative means run forever
 #iters = 32768 # measurements per batch
-iters = 16384 # measurements per batch
+#iters = 16384 # measurements per batch
+iters = 8192 # measurements per batch
 client.publish(topic="QTD/VDDG/tdc7201/batchsize",payload=str(iters))
+result_name = ("0","1","2","3","4","5","No calibration","INT1 fall timeout","TRIG1 fall timeout","INT1 early","TRIG1 rise timeout","START_MEAS active","TRIG1 active","INT1 active")
 while batches != 0:
     print("batches =",batches)
     client.loop()
+    resultDict = {}
     # Measure average time per measurement.
     then = time.time()
     resultList = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
@@ -89,11 +115,15 @@ while batches != 0:
         resultList[tdc.measure(simulate=True)] += 1
         # Clear interrupt register bits to prepare for next measurement.
         tdc.clear_status()
-    print(resultList)
-    client.publish(topic="QTD/VDDG/tdc7201/batch_results",payload=str(resultList))
+    #for i in range(len(resultList)):
+    #    resultDict[result_name[i]] = resultList[i]
+    #payload = json.dumps(resultDict)
+    payload = json.dumps(resultList)
+    print(payload)
+    client.publish(topic="QTD/VDDG/tdc7201/batch",payload=payload)
     now = time.time()
     duration = now - then
-    print(iters,"measurements in",duration,"seconds")
+    #print(iters,"measurements in",duration,"seconds")
     print((iters/duration),"measurements per second")
     #print((duration/iters),"seconds per measurement")
 
@@ -123,6 +153,7 @@ while batches != 0:
         #print("GPU temp =",gpu_temp)
         pass
     client.publish(topic="QTD/VDGG/qtd-0W/gpu_temp",payload=gpu_temp)
+
     batches -= 1
 
 # Turn the chip off.
