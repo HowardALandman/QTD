2e9ad32d476bca8c340a54aa9e8b4009203b7231
Howard A. Landman <howard@riverrock.org>
2020-05-25 13:43:25 -0600

    Delinted, and added basic docstrings.

--- src/qtd.py
+++ src/qtd.py
@@ -1,34 +1,38 @@
 #!/usr/bin/python3
+"""Run the (real or simulated) QTD experiment on a SPI-attached TDC7201 chip."""
 
 import time
-# sys for exit()
-import sys
 import os
-import tdc7201
-import paho.mqtt.client as mqtt
 import json
+import paho.mqtt.client as mqtt
+import tdc7201
 
-mqtt_server_name = "mqtt.eclipse.org"
+MQTT_SERVER_NAME = "mqtt.eclipse.org"
 
-# MQTT callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    #print("Connected with result code "+str(rc))
-    print("Connected to", mqtt_server_name, "with result code", rc)
+def on_connect(mqtt_client, userdata, flags, result_code):
+    """MQTT callback for when the client receives a CONNACK response from the server."""
+    print("Connected to", MQTT_SERVER_NAME, "with result code", result_code)
+    # Any subscribes should go here, so they get re-subscribed on a reconnect.
+
+# NEED an on_disconnect() method!
 
 client = mqtt.Client()
 client.on_connect = on_connect
-client.connect(mqtt_server_name, 1883, 60)
+client.connect(MQTT_SERVER_NAME, 1883, 60)
 
-def publish_tdc7201_driver(client):
+def publish_tdc7201_driver():
+    """Publish the version number of the TDC7201 driver to the MQTT server."""
     command = "python3 -m pip show tdc7201 | grep Version"
     try:
-        driver = os.popen(command).read().split()[1]
-    except:
+        with os.popen(command) as pipe:
+            driver = pipe.read().split()[1]
+    except IOError:
         driver = "unknown"
-    print("TDC7201 driver version =",driver)
-    client.publish(topic="QTD/VDGG/tdc7201/driver",payload=driver)
+    print("TDC7201 driver version =", driver)
+    client.publish(topic="QTD/VDGG/tdc7201/driver", payload=driver)
 
-def publish_uname(client):
+def publish_uname():
+    """Publish uname() data to the MQTT server."""
     # What hardware are we running on?
     uname = os.uname()
     nodename = uname.nodename
@@ -36,97 +40,106 @@ def publish_uname(client):
     sysname = uname.sysname
     release = uname.release
     version = uname.version
-    client.publish(topic="QTD/VDDG/"+nodename+"/nodename",payload=nodename,retain=True)
-    client.publish(topic="QTD/VDDG/"+nodename+"/machine",payload=machine,retain=True)
-    client.publish(topic="QTD/VDDG/"+nodename+"/OS",payload=sysname+" "+release+" "+version,retain=True)
+    client.publish(topic="QTD/VDDG/"+nodename+"/nodename",
+                   payload=nodename, retain=True)
+    client.publish(topic="QTD/VDDG/"+nodename+"/machine",
+                   payload=machine, retain=True)
+    client.publish(topic="QTD/VDDG/"+nodename+"/OS",
+                   payload=sysname+" "+release+" "+version, retain=True)
 
-temp = None
-
-def publish_cpu_temp(client,f = "/sys/class/thermal/thermal_zone0/temp"):
-    global temp
+def publish_cpu_temp(f_name="/sys/class/thermal/thermal_zone0/temp", __temp=None):
+    """Publish the CPU temperature to the MQTT server."""
+    # The __temp argument is a stupid way to create a persistent variable that
+    # is still private to this method. Do not assign to it as an argument.
+    # In many languages we could just say "static temp" to get that, but
+    # Python doesn't have that and neither "global" nor "nonlocal" are it.
     try:
-        cpu_temp_file = open(f)
-    except:
-        print("Couldn't open",f)
+        with open(f_name) as cpu_temp_file:
+            temp_line = list(cpu_temp_file)[0]
+    except IOError:
+        print("Couldn't open", cpu_temp_file)
     else:
-        temp_line = list(cpu_temp_file)[0]
-        #temp = int(temp_line)
         # Smooth
         new_temp = int(temp_line)
-        if temp is None:
-            temp = new_temp
+        if __temp is None:
+            __temp = new_temp
         else:
-            temp = (temp + new_temp) / 2
-        rounded = round(temp/1000,1)
-        #print("CPU temp =",rounded)
-    client.publish(topic="QTD/VDGG/qtd-0w/cpu_temp",payload=rounded)
+            __temp = (__temp + new_temp) / 2
+        rounded = round(__temp/1000, 1)
+        #print("CPU temp =", rounded)
+    client.publish(topic="QTD/VDGG/qtd-0w/cpu_temp", payload=rounded)
 
-def publish_gpu_temp(client,cmd = "vcgencmd measure_temp"):
+def publish_gpu_temp(cmd="vcgencmd measure_temp"):
+    """Publish the GPU temperature to the MQTT server."""
     try:
-        gpu_temp = (os.popen(cmd).read())[5:9]
-    except:
-        print("Running",cmd,"failed.")
+        with os.popen(cmd) as pipe:
+            gpu_temp = pipe.read()[5:9]
+    except IOError:
+        print("Running", cmd, "failed.")
         gpu_temp = "Failed"
     else:
-        #print("GPU temp =",gpu_temp)
+        #print("GPU temp =", gpu_temp)
         pass
-    client.publish(topic="QTD/VDGG/qtd-0w/gpu_temp",payload=gpu_temp)
+    client.publish(topic="QTD/VDGG/qtd-0w/gpu_temp", payload=gpu_temp)
 
-def publish_os_name(client,cmd = "head -1 /etc/os-release"):
+def publish_os_name(cmd="head -1 /etc/os-release"):
+    """Publish the OS name to the MQTT server."""
     # Get "pretty" OS name
     try:
-        os_name = os.popen(cmd).read().split('"')[1]
-        print("OS name =",os_name)
-    except:
+        with os.popen(cmd) as pipe:
+            os_name = pipe.read().split('"')[1]
+    except IOError:
+        print("Running", cmd, "failed.")
         os_name = "unknown"
-    client.publish(topic="QTD/VDDG/qtd-0w/os_name",payload=os_name,retain=True)
+    print("OS name =", os_name)
+    client.publish(topic="QTD/VDDG/qtd-0w/os_name", payload=os_name, retain=True)
 
-def publish_cpu_info(client,f = "/proc/cpuinfo"):
-    global nodename
+def publish_cpu_info(f_name="/proc/cpuinfo"):
+    """Publish the Raspberry Pi type name to the MQTT server."""
     try:
-        cpuinfo = open(f)
-    except:
-        print("Couldn't open",f)
+        with open(f_name) as cpu_info_file:
+            hw_list = list(cpu_info_file)[-4:]
+    except IOError:
+        print("Couldn't open", cpu_info_file)
     else:
-        hw = list(cpuinfo)[-4:]
-        for line in hw:
-            #print(line,end='')
+        for line in hw_list:
+            #print(line, end='')
             hw_pair = line.split()
             topic = "QTD/VDDG/qtd-0w/"+hw_pair[0]
             print(topic)
-            client.publish(topic=topic,payload=" ".join(hw_pair[2:]),retain=True)
+            client.publish(topic=topic, payload=" ".join(hw_pair[2:]), retain=True)
 
-publish_uname(client)
-publish_os_name(client)
-publish_cpu_info(client)
+publish_uname()
+publish_os_name()
+publish_cpu_info()
 
 tdc = tdc7201.TDC7201()	# Create TDC object with SPI interface.
 
 # Set RPi pin directions and default values for non-SPI signals.
 # These should stay the same for entire run.
 # This also puts the chip into reset ("off") state.
-tdc.initGPIO(trig2=None,int2=None)
+tdc.initGPIO(trig2=None, int2=None)
 
 # Setting and checking clock speed.
-tdc.set_SPI_clock_speed(tdc._maxSPIspeed)
+tdc.set_SPI_clock_speed(22500000)	# 22.5 MHz
 #tdc.set_SPI_clock_speed(tdc._maxSPIspeed // 2)
 
 # Internal timing
 #print("UNIX time settings:")
 #print("Epoch (time == 0):", time.asctime(time.gmtime(0)))
-now = time.time()
-#print("Time since epoch in seconds:", now)
-#print("Current time (UTC):", time.asctime(time.gmtime(now)))
-print("Current time (local):", time.asctime(time.localtime(now)), time.strftime("%Z"))
-#print("Time since reset asserted:", now - reset_start)
-publish_tdc7201_driver(client)	# Takes a few seconds.
+NOW = time.time()
+#print("Time since epoch in seconds:", NOW)
+#print("Current time (UTC):", time.asctime(time.gmtime(NOW)))
+print("Current time (local):", time.asctime(time.localtime(NOW)), time.strftime("%Z"))
+#print("Time since reset asserted:", NOW - reset_start)
+publish_tdc7201_driver()	# Takes a few seconds.
 #time.sleep(0.1)	# ensure a reasonable reset time
-#print("Time since reset asserted:", now - reset_start)
+#print("Time since reset asserted:", NOW - reset_start)
 
 # Turn the chip on.
-tdc.on(meas_mode=2,num_stop=5,clock_cntr_stop=1,timeout=0.0005)
-#tdc.on(meas_mode=1,num_stop=2,clock_cntr_stop=1,timeout=0.0005)
-client.publish(topic="QTD/VDDG/tdc7201/runstate",payload="ON")
+tdc.on(meas_mode=2, num_stop=5, clock_cntr_stop=1, timeout=0.0005)
+#tdc.on(meas_mode=1, num_stop=2, clock_cntr_stop=1, timeout=0.0005)
+client.publish(topic="QTD/VDDG/tdc7201/runstate", payload="ON")
 
 # Make sure our internal copy of the register state is up to date.
 #print("Reading chip side #1 register state:")
@@ -136,31 +149,33 @@ tdc.read_regs()
 batches = -1	# number of batches, negative means run forever
 #iters = 32768 # measurements per batch
 #iters = 16384 # measurements per batch
-iters = 8192 # measurements per batch
-client.publish(topic="QTD/VDDG/tdc7201/batchsize",payload=str(iters))
-result_name = ("0","1","2","3","4","5","No calibration","INT1 fall timeout","TRIG1 fall timeout","INT1 early","TRIG1 rise timeout","START_MEAS active","TRIG1 active","INT1 active")
+ITERS = 8192 # measurements per batch
+client.publish(topic="QTD/VDDG/tdc7201/batchsize", payload=str(ITERS))
+#RESULT_NAME = ("0", "1", "2", "3", "4", "5",
+#               "No calibration", "INT1 fall timeout", "TRIG1 fall timeout",
+#               "INT1 early", "TRIG1 rise timeout", "START_MEAS active",
+#               "TRIG1 active", "INT1 active")
 while batches != 0:
-    print("batches =",batches)
+    print("batches =", batches)
     client.loop()
-    resultDict = {}
+    #resultDict = {}
     # Measure average time per measurement.
-    then = time.time()
-    resultList = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    for m in range(iters):
-        resultList[tdc.measure(simulate=True)] += 1
+    THEN = time.time()
+    result_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for m in range(ITERS):
+        result_list[tdc.measure(simulate=True)] += 1
         # Clear interrupt register bits to prepare for next measurement.
         tdc.clear_status()
-    #for i in range(len(resultList)):
-    #    resultDict[result_name[i]] = resultList[i]
-    #payload = json.dumps(resultDict)
-    payload = json.dumps(resultList)
-    print(payload)
-    client.publish(topic="QTD/VDDG/tdc7201/batch",payload=payload)
-    now = time.time()
-    duration = now - then
-    #print(iters,"measurements in",duration,"seconds")
-    print((iters/duration),"measurements per second")
-    #print((duration/iters),"seconds per measurement")
+    #for i in range(len(result_list)):
+    #    resultDict[RESULT_NAME[i]] = result_list[i]
+    PAYLOAD = json.dumps(result_list)
+    print(PAYLOAD)
+    client.publish(topic="QTD/VDDG/tdc7201/batch", payload=PAYLOAD)
+    NOW = time.time()
+    DURATION = NOW - THEN
+    #print(ITERS, "measurements in", DURATION, "seconds")
+    print((ITERS/DURATION), "measurements per second")
+    #print((DURATION/ITERS), "seconds per measurement")
 
     publish_cpu_temp(client)
 
@@ -168,4 +183,4 @@ while batches != 0:
 
 # Turn the chip off.
 tdc.off()
-client.publish(topic="QTD/VDDG/tdc7201/runstate",payload="OFF")
+client.publish(topic="QTD/VDDG/tdc7201/runstate", payload="OFF")
